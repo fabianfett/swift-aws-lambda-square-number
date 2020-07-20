17118e61cc394063d306203147ec786e6566eb71
Fabian Fett <fabianfett@mac.com>
2020-07-20 12:34:05 +0200

    Update for version 0.2.0

--- Sources/SquareNumber/main.swift
+++ Sources/SquareNumber/main.swift
@@ -8,14 +8,6 @@ struct Output: Codable {
   let result: Double
 }
 
-#if DEBUG
-try Lambda.withLocalServer {
-  Lambda.run { (context, input: Input, callback) in
-    callback(.success(Output(result: input.number * input.number)))
-  }
-}
-#else
 Lambda.run { (context, input: Input, callback) in
   callback(.success(Output(result: input.number * input.number)))
 }
-#endif
